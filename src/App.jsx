acbfb39963cc318d5e15ecc971ee968b88a281f7
theigniteai <theignite.ai@gmail.com>
2025-06-08 18:39:16 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,15 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Routes */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="create-project" element={<CreateProject />} />
+          {/* Future pages like /leads or /settings can go here */}
         </Route>
       </Routes>
     </Router>
